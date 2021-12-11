8172164ac362f3473bfea5fd4b8d47827c0b1b7b
Daniel Carmel <danielcarmel6@gmail.com>
2021-12-11 23:14:15 +0200

    retrieve all liked videos working + pagination

--- main.go
+++ main.go
@@ -8,34 +8,54 @@ import (
 
 const MaxResults = 5
 const MyRating = "like"
+const VideoCategoryId = "10" // Youtube api id for music videos
 
 func main() {
+	// Create client
 	client := api.GetClient()
 	service, err := youtube.New(client)
 	if err != nil {
 		log.Fatalf("Error creating YouTube client: %v", err)
 	}
 
-	var likeVideos []*youtube.Video
-	youtubeItemPart := []string{"topicDetails", "id"}
-	response, err := service.Videos.List(youtubeItemPart).MaxResults(MaxResults).MyRating(MyRating).Do()
-	likeVideos = append(likeVideos, response.Items...)
-
-	for response.NextPageToken != "" {
-		response, err := service.Videos.List(youtubeItemPart).MaxResults(MaxResults).MyRating(MyRating).PageToken(response.NextPageToken).Do()
-		if err != nil {
-			log.Fatalf("Error listing videos: %v", err)
-		}
-		likeVideos = append(likeVideos, response.Items...)
+	likedVideos, err := GetLikedVideos(service)
+	if err != nil {
+		log.Fatalf("Error listing videos: %v", err)
 	}
+
 	// Iterate through each item and add it to the correct list.
-	for _, item := range response.Items {
-		if item.Snippet.CategoryId == "10" { // Youtube`s id for music videos
-			log.Println(item)
-		} else {
-			log.Println("item has ", item.Snippet.CategoryId)
-			continue
-		}
+	for _, item := range likedVideos {
+		println(item)
+	}
+}
+
+func GetLikedVideos(service *youtube.Service) ([]*youtube.Video, error) {
+	//var isItStillGoing = true
+	var videosList []*youtube.Video
+	youtubeItemPart := []string{"id", "snippet", "topicDetails"}
+
+	response, err := service.Videos.List(youtubeItemPart).MaxResults(MaxResults).VideoCategoryId(VideoCategoryId).MyRating(MyRating).Do()
+	if err != nil {
+		return nil, err
 	}
 
+	//NextPageToken := response.NextPageToken
+	videosList = append(videosList, response.Items...)
+
+	/*
+		for isItStillGoing {
+			response, err := service.Videos.List(youtubeItemPart).MaxResults(MaxResults).VideoCategoryId(VideoCategoryId).MyRating(MyRating).PageToken(NextPageToken).Do()
+			if err != nil {
+				return nil, err
+			}
+			videosList = append(videosList, response.Items...)
+
+			if response.NextPageToken == "" {
+				isItStillGoing = false
+			} else {
+				NextPageToken = response.NextPageToken
+			}
+		}
+	*/
+	return videosList, nil
 }
